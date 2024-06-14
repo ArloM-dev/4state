1fbaf0c2932e1ae2f8edd496e623b9a9e0b3a83b
ArloM <130303465+ArloM-dev@users.noreply.github.com>
2024-06-14 16:56:13 +0100

    started better piece generation

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -14,11 +14,12 @@ public class Board : MonoBehaviour
     public Material material;
     
     // declairing all game pieces
-    public GameObject piece0, piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8, piece9, piece10, piece11, piece12, piece13, piece14, piece15;
+    public Sprite piece0, piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8, piece9, piece10, piece11, piece12, piece13, piece14, piece15;
 
     // Start is called before the first frame update
     void Start()
     {
+        Createpieces();
         BuildGrid();
         DisplayGrid(grid);
     }
@@ -121,8 +122,7 @@ public class Board : MonoBehaviour
 
     public void DisplayGrid(int[] grid)
     {
-        GameObject[] piece = {piece0, piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8, piece9, piece10, piece11, piece12, piece13, piece14, piece15};
-        
+        GameObject[] pieces = Createpieces().pieces;
         //sets pieces to grid
         for (int pos = 0; pos < 16; pos++)
         {
@@ -130,8 +130,8 @@ public class Board : MonoBehaviour
             {
                 float x = (float)(gridsize * (pos % 4 - 1.5));
                 float y = (float)(gridsize * (1.5 - ((pos - pos % 4) / 4)));
-                piece[grid[pos]].SetActive(true);
-                piece[grid[pos]].transform.position = new Vector3(x, y, -1);
+                pieces[grid[pos]].SetActive(true);
+                pieces[grid[pos]].transform.position = new Vector3(x, y, -1);
             }
             //finds unused pieces and hides them
             for (int i = 0; i < 16; i++)
@@ -147,9 +147,22 @@ public class Board : MonoBehaviour
                 }
                 if (incol == false)
                 {
-                    piece[i].SetActive(false);
+                    pieces[i].SetActive(false);
                 }
             }
         }
     }
+
+    public void Createpieces()
+    {
+        Sprite[] piecesprite = {piece0, piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8, piece9, piece10, piece11, piece12, piece13, piece14, piece15};
+        GameObject[] pieces = new GameObject[16];
+        for (int piece = 0; piece < 16; piece++)
+        {
+            GameObject currentpiece = pieces[piece];
+            currentpiece = new GameObject("piece" + piece.ToString(), typeof(SpriteRenderer), typeof(BoxCollider2D), typeof(pieces));
+            currentpiece.GetComponent<SpriteRenderer>().sprite = piecesprite[piece];
+        }
+    }
+
 }
